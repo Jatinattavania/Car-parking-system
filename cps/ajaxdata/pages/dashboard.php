300180564b91576b55f8e7cd667159772351303b
nishant32400 <67227861+nishant32400@users.noreply.github.com>
2022-06-02 12:14:19 +0530

    added pages/dashboard.php
    line37 class is added breadcrumb-row

--- cps/ajaxdata/pages/dashboard.php
+++ cps/ajaxdata/pages/dashboard.php
@@ -34,7 +34,7 @@ if (strlen($_SESSION['users']=="Name")) {
 		?>
 		
 	<!-- <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main"> -->
-		<div class="row" style="margin-top:50px;">
+		<div class="row breadcrumb-row" style="margin-top:50px;">
 			<ol class="breadcrumb">
 				<li><a href="#">
 					<em class="fa fa-home"></em>
